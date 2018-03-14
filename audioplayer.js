f899359b735abd6990c2718b933687604686b30b
JAVASCRlPT <sahrulsidik000@gmail.com>
2018-03-14 12:22:07 +0700

    Update audioplayer.js

--- audioplayer.js
+++ audioplayer.js
@@ -1,52 +1,64 @@
-$(function() {
+/*
+DONE:
+- play/pause;
+- volume;
+- progress bar.
 
-	var audio = $("audio")[0];
+TODO:
+- backward/forward;
+- repeat/shuffle;
+- lyrics;
+- playlist.
+*/
 
-	$('#btn-play-pause').on('click', function() {
-		//Play/pause the track
-		if (audio.paused == false) {
-			audio.pause();
-			$(this).children('i').removeClass('fa-pause');
-			$(this).children('i').addClass('fa-play');
-		} else {
-			audio.play();
-			$(this).children('i').removeClass('fa-play');
-			$(this).children('i').addClass('fa-pause');
-		}
-	});
+var audio = document.getElementById('audio');
+var progress = document.getElementById('progress');
+var playpause = document.getElementById("play-pause");
+var volume = document.getElementById("volume");
 
-	$('#btn-stop').on('click', function() {
-		//Stop the track
-		audio.pause();
-		audio.currentTime = 0;
-		$('#btn-play-pause').children('i').removeClass('fa-pause');
-		$('#btn-play-pause').children('i').addClass('fa-play');
-	});
-	
-	$('#btn-mute').on('click', function() {
-		//Mutes/unmutes the sound
-		if(audio.volume != 0) {
-			audio.volume = 0;
-			$(this).children('i').removeClass('fa-volume-off');
-			$(this).children('i').addClass('fa-volume-up');
-		} else {
-			audio.volume = 1;
-			$(this).children('i').removeClass('fa-volume-up');
-			$(this).children('i').addClass('fa-volume-off');
-		}
-	});
+audio.controls = false;
 
-	function updateProgress() {
-		//Updates the progress bar
-		var progress = document.getElementById("progress");
-		var value = 0;
-		if (audio.currentTime > 0) {
-			value = Math.floor((100 / audio.duration) * audio.currentTime);
-		}
-		progress.style.width = value + "%";
-	}
+audio.addEventListener('timeupdate', function() {
+  	updateProgress();
+}, false);
 
-	//Progress Bar event listener
-	audio.addEventListener("timeupdate", updateProgress, false);
-	
-});
+function togglePlayPause() {
+   if (audio.paused || audio.ended) {
+      playpause.title = "Pause";
+      playpause.innerHTML = '<i class="fa fa-pause fa-3x"></i>';
+      audio.play();
+   } else {
+      playpause.title = "Play";
+      playpause.innerHTML = '<i class="fa fa-play fa-3x"></i>';
+      audio.pause();
+   }
+}
+
+function setVolume() {
+   audio.volume = volume.value;
+}
+
+function updateProgress() {
+	var percent = Math.floor((100 / audio.duration) * audio.currentTime);
+	progress.value = percent;
+	var canvas = document.getElementById('progress');
+	var context = canvas.getContext('2d');
+	var centerX = canvas.width / 2;
+	var centerY = canvas.height / 2;
+	var radius = 150;
+	var circ = Math.PI * 2;
+	var quart = Math.PI / 2;
+	var cpercent = percent / 100; /* current percent */
+	context.beginPath();
+	context.arc(centerX, centerY, radius, 0, ((circ) * cpercent), false);
+	context.lineWidth = 10;
+	context.strokeStyle = '#26C5CB';
+	context.stroke();
+	if (audio.ended) resetPlayer();
+}
+
+function resetPlayer() {
+	  audio.currentTime = 0; context.clearRect(0,0,canvas.width,canvas.height);
+  playpause.title = "Play";
+	  playpause.innerHTML = '<i class="fa fa-play fa-3x"></i>';
+}
