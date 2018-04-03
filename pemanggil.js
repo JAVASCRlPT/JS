3060992e38613c53cdbd4d459f2709556d819c22
JAVASCRlPT <sahrulsidik000@gmail.com>
2018-04-03 09:40:39 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -20,10 +20,11 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*2)+ 1);
+var x=Math.floor((Math.random()*3)+ 1);
 var xxx=null;
 if(x=="1"){xxx="/p/bisnis-uang-bitcoin-dollar-gratis.html"}
-if(x=="2"){xxx="/p/keindahan-raja-ampat-yang-wajib-anda.html"}a_to_ve[i].href="https://4usafelink.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="2"){xxx="/p/keindahan-raja-ampat-yang-wajib-anda.html"}
+if(x=="3"){xxx="/p/go-to-url.html"}a_to_ve[i].href="https://4usafelink.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
