2c890f87788d501b120d1d2cc0c448aae17a2ec3
JAVASCRlPT <sahrulsidik000@gmail.com>
2018-04-03 09:37:31 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -22,7 +22,7 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*2)+ 1);
 var xxx=null;
-if(x=="1"){xxx="/p/penyebab-munculnya-jerawat.html"}
+if(x=="1"){xxx="/p/bisnis-uang-bitcoin-dollar-gratis.html"}
 if(x=="2"){xxx="/p/keindahan-raja-ampat-yang-wajib-anda.html"}a_to_ve[i].href="https://4usafelink.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
